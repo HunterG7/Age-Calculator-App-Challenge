a20453928af5ef7481d09b1fef985ad9bf05d33f
hunter grubbs <hunter.grubbs07@gmail.com>
2023-04-22 23:08:31 -0500

    completed

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
 "use strict";
 
-//query selectors
+//variables
 const submitBtn = document.querySelector(".img-wrapper");
 
 const dayErrorParent = document.querySelector("#day-error");
 const monthErrorParent = document.querySelector("#month-error");
 const yearErrorParent = document.querySelector("#year-error");
 
-const dayDisplayParent = document.querySelector("#day-row p");
-const monthDisplayParent = document.querySelector("#month-row p");
-const yearDisplayParent = document.querySelector("#year-row p");
+const dayDisplay = document.querySelector("#day-row p");
+const monthDisplay = document.querySelector("#month-row p");
+const yearDisplay = document.querySelector("#year-row p");
+
+// use these to style red border / label when error occurs
+const dayLabel = document.querySelector("#day-input-box h2");
+const monthLabel = document.querySelector("#month-input-box h2");
+const yearLabel = document.querySelector("#year-input-box h2");
+
+const dayInputBox = document.querySelector("#day-value");
+const monthInputBox = document.querySelector("#month-value");
+const yearInputBox = document.querySelector("#year-value");
+
+const labels = document.querySelectorAll(".input-box h2");
+const inputBoxes = document.querySelectorAll(".input-box input");
 
 // event listener
 submitBtn.addEventListener('click', (e)=>{
     dayErrorParent.innerHTML = "";
     monthErrorParent.innerHTML = "";
     yearErrorParent.innerHTML = "";
+
+    labels.forEach((label)=>{
+        label.style.color = "var(--smokey-grey)";
+    });
+    inputBoxes.forEach((inputBox)=>{
+        inputBox.style.borderColor = "var(--light-grey)";
+    });
 
     let day = document.querySelector("#day-value").value;
     let month = document.querySelector("#month-value").value;
@@ -28,15 +47,35 @@ submitBtn.addEventListener('click', (e)=>{
 
 // calculates results and renders them onto page functions
 const calculateResults = (day, month, year) => {
+    const inputDate = new Date(`${year}-${month}-${day}`);
+    const currentDate = new Date();
 
+    if (inputDate > currentDate) {
+        futureDateError();
+    } else {
+        let yearDiff = currentDate.getFullYear() - inputDate.getFullYear();
+        let monthDiff = currentDate.getMonth() - inputDate.getMonth();
+        let dayDiff = currentDate.getDate() - inputDate.getDate();
 
+        // Adjust the year and month difference if the day difference is negative
+        if (dayDiff < 0) {
+            monthDiff--;
+            const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+            dayDiff += daysInMonth;
+        }
+        if (monthDiff < 0) {
+            yearDiff--;
+            monthDiff += 12;
+        }
 
-
-    renderResults();
+        renderResults(dayDiff,monthDiff, yearDiff);
+    }
 }
 
 const renderResults = (day, month, year) => {
-
+    dayDisplay.innerHTML = `<p>${day}</p>`;
+    monthDisplay.innerHTML = `<p>${month}</p>`;
+    yearDisplay.innerHTML = `<p>${year}</p>`;
 }
 
 
@@ -46,8 +85,9 @@ const dayError = (parent) =>{
     errorElement.innerHTML = `
         <p class="error-text">Must be a valid day</p> 
     `;
-    console.log(parent);
     parent.append(errorElement);
+    dayLabel.style.color = "var(--light-red)";
+    dayInputBox.style.borderColor = "var(--light-red)";
 }
 
 const monthError = (parent) => {
@@ -55,8 +95,9 @@ const monthError = (parent) => {
     errorElement.innerHTML = `
         <p class="error-text">Must be a valid month</p> 
     `;
-    console.log(parent);
     parent.append(errorElement);
+    monthLabel.style.color = "var(--light-red)";
+    monthInputBox.style.borderColor = "var(--light-red)";
 }
 
 const yearError = (parent) => {
@@ -64,17 +105,34 @@ const yearError = (parent) => {
     errorElement.innerHTML = `
         <p class="error-text">Must be a valid year</p> 
     `;
-    console.log(parent);
     parent.append(errorElement);
+    yearLabel.style.color = "var(--light-red)";
+    yearInputBox.style.borderColor = "var(--light-red)";
 }
 
-const emptyFieldError = (parent) => {
+const emptyFieldError = (parent, label, inputBox) => {
     let errorElement = document.createElement("div");
     errorElement.innerHTML = `
         <p class="error-text">This field is required</p> 
     `;
-    console.log(parent);
     parent.append(errorElement);
+    label.style.color = "var(--light-red)";
+    inputBox.style.borderColor = "var(--light-red)";
+}
+
+const futureDateError = () => {
+    let errorElement = document.createElement("div");
+    errorElement.innerHTML = `
+        <p class="error-text">Must be a valid date</p> 
+    `;
+    dayErrorParent.append(errorElement);
+
+    labels.forEach((label)=>{
+        label.style.color = "var(--light-red)";
+    });
+    inputBoxes.forEach((inputBox)=>{
+        inputBox.style.borderColor = "var(--light-red)";
+    });
 }
 
 
@@ -86,14 +144,18 @@ const dayMonthValidation = (dayStr, monthStr, yearStr, validationCount) => {
 
     // checks basic day and month conditions
     if (monthStr.trim() === ""){
-        emptyFieldError(monthErrorParent);
+        emptyFieldError(monthErrorParent, monthLabel, monthInputBox);
+        validationCount--;
     } else if (isNaN(month) || month < 1 || month > 12){
         monthError(monthErrorParent);
+        validationCount--;
     }
     if (dayStr.trim() === ""){
-        emptyFieldError(dayErrorParent);
+        emptyFieldError(dayErrorParent, dayLabel, dayInputBox);
+        validationCount--;
     } else if (isNaN(day) || day < 1 || day > 31){
         dayError(dayErrorParent);
+        validationCount--;
     }
 
     // checks num of days based on year and month
@@ -105,6 +167,7 @@ const dayMonthValidation = (dayStr, monthStr, yearStr, validationCount) => {
                     validationCount++;
                 } else {
                     dayError(dayErrorParent);
+                    validationCount--;
                 }
                 yearValidation(day, month, yearStr, validationCount);
                 break;
@@ -113,6 +176,7 @@ const dayMonthValidation = (dayStr, monthStr, yearStr, validationCount) => {
                     validationCount++;
                 } else {
                     dayError(dayErrorParent);
+                    validationCount--;
                 }
                 yearValidation(day, month, yearStr, validationCount);
                 break;
@@ -126,6 +190,7 @@ const dayMonthValidation = (dayStr, monthStr, yearStr, validationCount) => {
                 validationCount++;
             } else {
                 dayError(dayErrorParent);
+                validationCount--;
             }
             yearValidation(day, month, yearStr, validationCount);
             break;
@@ -139,50 +204,19 @@ const dayMonthValidation = (dayStr, monthStr, yearStr, validationCount) => {
 const yearValidation = (day, month, yearStr, validationCount) => {
     let year = parseInt(yearStr);
     if (yearStr.trim() === "") {
-        emptyFieldError(yearErrorParent);
-    } else if (isNaN(year) || year < 1 || year > 2023) {
+        emptyFieldError(yearErrorParent, yearLabel, yearInputBox);
+        validationCount--;
+    } else if (isNaN(year) || year < 1) {
         yearError(yearErrorParent);
+        validationCount--;
     } else {
         validationCount++;
     }
 
+    console.log(validationCount);
     if (validationCount === 2){
         calculateResults(day, month, year);
     }
 }
 
-// CHATGPT CODE
-// const calculateAgeDifference = (date1, date2) => {
-//     const d1 = new Date(date1.year, date1.month - 1, date1.day);
-//     const d2 = new Date(date2.year, date2.month - 1, date2.day);
-//     const currentDate = new Date();
-//
-//     if (d1 > currentDate) {
-//         return "Date 1 is in the future";
-//     } else if (d2 > currentDate) {
-//         return "Date 2 is in the future";
-//     } else if (d1 > d2) {
-//         return "Date 1 is later than date 2";
-//     } else {
-//         let years = d2.getFullYear() - d1.getFullYear();
-//         let months = d2.getMonth() - d1.getMonth();
-//         let days = d2.getDate() - d1.getDate();
-//
-//         if (months < 0) {
-//             years--;
-//             months += 12;
-//         }
-//
-//         if (days < 0) {
-//             months--;
-//             const daysInPrevMonth = new Date(
-//                 d2.getFullYear(),
-//                 d2.getMonth(),
-//                 0
-//             ).getDate();
-//             days += daysInPrevMonth;
-//         }
-//
-//         return `${years} years, ${months} months, and ${days} days`;
-//     }
-// };
+
